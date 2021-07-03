62ae6dbf47f914aee33a8366e16aefccb1b75b1d
palemao <74777355+palemao@users.noreply.github.com>
2021-07-03 19:49:23 +0100

    Update motorencoders.c

--- motorencoders.c
+++ motorencoders.c
@@ -24,6 +24,10 @@
 /* USER CODE BEGIN Includes */
 #include <stdio.h>
 #include <string.h>
+#include <string.h>
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -41,15 +45,43 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
+TIM_HandleTypeDef htim6;
 TIM_HandleTypeDef htim9;
+TIM_HandleTypeDef htim11;
 
 UART_HandleTypeDef huart2;
 
 /* USER CODE BEGIN PV */
 uint8_t snum[100];
-uint8_t direction[20];
-int pulseticks=0;
-int revoultions=0;
+uint8_t direction[100];
+float pulseticks=0;
+float revoultions=0;
+float ovpulsetick =0;
+int anticlockwiserev;
+int clockwiserev;
+int turns = 0;
+int i=0;
+float brake=0;
+float command=0;
+float timer_val = 0;
+float timelapsed =0;
+float maxsecondscount =0;
+int revostop =101;
+float storeovopulse =0;
+float RPM =0;
+float PWMsig=0;
+float rpm_d=5.0;
+float e=0, e_prev=0, inte_prev=0, inte=0,dt=0,theta=0,theta_prev=0;
+float V=0,timelapsedprev=0;
+float kp=800, ki=50, kd=100;
+int avgcount=0;
+float sumstoreovopul=0;
+
+
+float ovopulstickprev=0, storeovopul=0, storeovopulprev=0, chnge=0;
+int pulseininita = 44000;
+int count=0, countprev=0;
+float storeovopulog=0, RPMog=0;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -57,6 +89,8 @@ void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_USART2_UART_Init(void);
 static void MX_TIM9_Init(void);
+static void MX_TIM11_Init(void);
+static void MX_TIM6_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -96,26 +130,80 @@ int main(void)
   MX_GPIO_Init();
   MX_USART2_UART_Init();
   MX_TIM9_Init();
+  MX_TIM11_Init();
+  MX_TIM6_Init();
   /* USER CODE BEGIN 2 */
 
+ HAL_Delay(5000);
 
- HAL_GPIO_WritePin (GPIOA, GPIO_PIN_8, GPIO_PIN_SET);
- HAL_GPIO_WritePin (GPIOB, GPIO_PIN_5, GPIO_PIN_RESET);
+ HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_6);
+ HAL_Delay(1000);
+ HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_6);
+ HAL_Delay(1000);
+ HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_6);
+ HAL_Delay(100);
 
-HAL_TIM_PWM_Start(&htim9, TIM_CHANNEL_1);
-//HAL_TIM_PWM_Start(&htim11, TIM_CHANNEL_1);
 
+ HAL_GPIO_WritePin(GPIOA, GPIO_PIN_8, GPIO_PIN_SET);
+ HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, GPIO_PIN_RESET);
+ HAL_TIM_PWM_Start(&htim9, TIM_CHANNEL_1);
+ HAL_TIM_Base_Start_IT(&htim11);
 
+ HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, GPIO_PIN_RESET);
 
+ HAL_TIM_Base_Start_IT(&htim6);
+ __HAL_TIM_GET_COUNTER(&htim6)=0;
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-	  HAL_Delay(25);
-	  HAL_UART_Transmit(&huart2, direction, strlen((char*)direction), HAL_MAX_DELAY);
 
+	  sprintf((char*)snum, "%.2f,%.2f,%.2f,%.2f\r \n ",RPM,V, e,inte);
+	   if(count>countprev && avgcount % 5!=0){
+		  		//timelapsed = ((__HAL_TIM_GET_COUNTER(&htim6)*0.0001)+maxsecondscount-6.5536-0.462916)/1.0138291;
+		  		sumstoreovopul += ovpulsetick-ovopulstickprev;
+		  		storeovopulog = ovpulsetick-ovopulstickprev;
+		  		avgcount++;
+		  		ovopulstickprev=ovpulsetick;
+		  		countprev=count;
+
+	  }
+	  if(avgcount % 5==0){
+		  timelapsed = ((__HAL_TIM_GET_COUNTER(&htim6)*0.0001)+maxsecondscount-6.5536-0.462916)/1.0138291;
+		  RPM=sumstoreovopul/4;
+		   dt= timelapsed - timelapsedprev;
+	        e=rpm_d-RPM;
+	          inte = inte_prev + (dt * (e + e_prev) / 2);
+	          V += kp * e + ki * inte + (kd * (e - e_prev) / dt);
+	          __HAL_TIM_SET_COMPARE(&htim9, TIM_CHANNEL_1, V);
+
+	          e_prev=e;
+	          inte_prev=inte;
+	          theta_prev=theta;
+	          timelapsedprev =timelapsed;
+	          sumstoreovopul =0;
+
+	          ovopulstickprev=ovpulsetick;
+
+
+
+
+	          avgcount++;
+	    	//  sprintf((char*)snum, "%.5f, %.5f, %.2f, %.2f, %.2f\r \n ",RPM, RPMog, ovpulsetick, PWMsig, chnge);
+
+
+	  	}
+
+
+
+	 // sprintf((char*)snum, "%.2f,%.2f,%.2f,\r \n ",timelapsed,revoultions,ovpulsetick);
+
+	 // sprintf((char*)snum, "%i\r \n ",PWMsig);
+	  HAL_UART_Transmit(&huart2, snum, strlen((char*)snum), HAL_MAX_DELAY);
+	  //HAL_Delay(25);b
+	 // HAL_UART_Transmit(&huart2, direction, strlen((char*)direction), HAL_MAX_DELAY);
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -166,6 +254,44 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
+}
+
+/**
+  * @brief TIM6 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM6_Init(void)
+{
+
+  /* USER CODE BEGIN TIM6_Init 0 */
+
+  /* USER CODE END TIM6_Init 0 */
+
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+
+  /* USER CODE BEGIN TIM6_Init 1 */
+
+  /* USER CODE END TIM6_Init 1 */
+  htim6.Instance = TIM6;
+  htim6.Init.Prescaler = 8400-1;
+  htim6.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim6.Init.Period = 65536-1;
+  htim6.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim6) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim6, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM6_Init 2 */
+
+  /* USER CODE END TIM6_Init 2 */
+
 }
 
 /**
@@ -221,6 +347,37 @@ static void MX_TIM9_Init(void)
 }
 
 /**
+  * @brief TIM11 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM11_Init(void)
+{
+
+  /* USER CODE BEGIN TIM11_Init 0 */
+
+  /* USER CODE END TIM11_Init 0 */
+
+  /* USER CODE BEGIN TIM11_Init 1 */
+
+  /* USER CODE END TIM11_Init 1 */
+  htim11.Instance = TIM11;
+  htim11.Init.Prescaler = 8400-1;
+  htim11.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim11.Init.Period = 500-1;
+  htim11.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim11.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim11) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM11_Init 2 */
+
+  /* USER CODE END TIM11_Init 2 */
+
+}
+
+/**
   * @brief USART2 Initialization Function
   * @param None
   * @retval None
@@ -269,7 +426,7 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_8, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6|GPIO_PIN_8, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, GPIO_PIN_RESET);
@@ -280,18 +437,18 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
 
+  /*Configure GPIO pins : PA6 PA8 */
+  GPIO_InitStruct.Pin = GPIO_PIN_6|GPIO_PIN_8;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
   /*Configure GPIO pin : PB10 */
   GPIO_InitStruct.Pin = GPIO_PIN_10;
   GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
-
-  /*Configure GPIO pin : PA8 */
-  GPIO_InitStruct.Pin = GPIO_PIN_8;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
   /*Configure GPIO pin : PB4 */
   GPIO_InitStruct.Pin = GPIO_PIN_4;
@@ -314,59 +471,62 @@ static void MX_GPIO_Init(void)
 
 /* USER CODE BEGIN 4 */
 
+
 void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin){
 
-	if(GPIO_Pin == GPIO_PIN_4 && pulseticks<224){
-		pulseticks++;
-				if(HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_10)>0){
-					  		 sprintf((char*)direction, "anticlockwise \r \n");}
-				else if (HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_10)==0)
-				{
-					  				sprintf((char*)direction, "clockwise \r \n");
-					  			}
+	if(GPIO_Pin == GPIO_PIN_4){
+
+	ovpulsetick++;
 	}
-	else if(GPIO_Pin == GPIO_PIN_4 && pulseticks>=224) {
-				  revoultions++;
-				  pulseticks=0;
-			  		}
+
+	/*if(GPIO_Pin == GPIO_PIN_4){
+		timelapsed = ((__HAL_TIM_GET_COUNTER(&htim6)*0.0001)+maxsecondscount-6.5536-0.462916)/1.0138291;
+
+					if(HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_10)>0 && revoultions<revostop && revoultions>-revostop){
+								pulseticks++;
+								ovpulsetick++;
+								if(pulseticks>223){
+									revoultions++;
+									pulseticks=0;
+
+								}
+
+					}
+					else if(HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_10)==0 && revoultions<revostop && revoultions>-revostop){
+									pulseticks--;
+									ovpulsetick--;
+									if(pulseticks<-223){
+										revoultions--;
+										pulseticks=0;
+									}
+
+										}
+					else if(revoultions>revostop-1 || revoultions<-revostop+1){ //brakes after specified revolutions
+						 HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, GPIO_PIN_SET);
+						 HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, GPIO_PIN_SET);
+						 HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_6);
+						 command=1;
+					}
+	}*/
 }
 
-/*void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin){
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef * htim){
 
-	if(GPIO_Pin == GPIO_PIN_4){
-				if(HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_10)>0){
-							pulseticks++;
-							if(pulseticks>=224){
-								revoultions++;
-								anticlockwiserev++;
-								pulseticks =1;
-								theta= revoultions+pulseticks/224;
-								sprintf((char*)direction, "theta=%.2f \r \n", theta);
-								HAL_UART_Transmit(&huart2, direction, strlen((char*)direction), HAL_MAX_DELAY);
-							}
-							theta= revoultions+pulseticks/224;
-					sprintf((char*)direction, "theta=%.2f \r \n", theta);
-					HAL_UART_Transmit(&huart2, direction, strlen((char*)direction), HAL_MAX_DELAY);}
-				else if (HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_10)==0)
-				{           pulseticks--;
-										if(pulseticks<=-224){
-												revoultions--;
-												clockwiserev++;
-												pulseticks =-1;
-												theta= revoultions+pulseticks/224;
-												sprintf((char*)direction, "theta=%.2f \r \n", theta);
-												HAL_UART_Transmit(&huart2, direction, strlen((char*)direction), HAL_MAX_DELAY);
-											}
-										theta= revoultions+pulseticks/224;
-					  				sprintf((char*)direction, "theta=%.2f\r \n", theta);
-					  				HAL_UART_Transmit(&huart2, direction, strlen((char*)direction), HAL_MAX_DELAY);
-					  			}
+	if(htim == &htim11){
+
+		count++;
 	}
+	if(htim == &htim6){
+		maxsecondscount += 6.5536;
+		//HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_6);
+		/*	PWMsig+=5000;
+		if(PWMsig < 50000){
+		__HAL_TIM_SET_COMPARE(&htim9, TIM_CHANNEL_1, PWMsig);
+		//sprintf((char*)snum, "%.2f,%.2f,%.2f\r \n ",PWMsig,ovpulsetick,RPM);
+	}*/
 
-}*/
-
-
-
+	}
+}
 /* USER CODE END 4 */
 
 /**
